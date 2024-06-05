a6aa3dfe4c94c563ef1e96a6cf246249a169935d
mario-ito <marioib77@gmail.com>
2024-06-04 22:49:58 -0300

    disparando deploy

--- src/main/java/br/com/aquacare/api_aquacare/ApiAquacareApplication.java
+++ src/main/java/br/com/aquacare/api_aquacare/ApiAquacareApplication.java
@@ -2,7 +2,7 @@ package br.com.aquacare.api_aquacare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// disparando deploy
 @SpringBootApplication
 public class ApiAquacareApplication {
 
